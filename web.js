15d52bbdb5f5ce79c72156eb6e050c41ff718be2
Linjun Hu <aimee.hlj@gmail.com>
2013-07-17 05:15:33 +0000

    updated web.js, making it read file from disk

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buf = new Buffer(fs.readFileSync('indext.html'),'utf-8');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
