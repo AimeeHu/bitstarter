7caf42b6d36d9e0d38e3dbf89ef8d523078fa187
Linjun Hu <aimee.hlj@gmail.com>
2013-07-17 05:21:45 +0000

    debug

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var buf = new Buffer(fs.readFileSync('indext.html'),'utf-8');
+var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
 
 app.get('/', function(request, response) {
   response.send(buf.toString());
